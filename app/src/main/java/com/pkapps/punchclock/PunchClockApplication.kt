09e50d6df141182c77a7af587faf6a5c98bb317c
Peter Kastner <peter.kastner92@gmail.com>
2023-03-30 16:53:02 +0200

    Move into android block and add curly braces

--- app/src/main/java/com/pkapps/punchclock/PunchClockApplication.kt
+++ app/src/main/java/com/pkapps/punchclock/PunchClockApplication.kt
@@ -4,4 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PunchClockApplication : Application()
+class PunchClockApplication : Application() {
+
+}
