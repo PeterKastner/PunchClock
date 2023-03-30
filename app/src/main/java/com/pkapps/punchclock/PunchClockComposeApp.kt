5022f08205c20306e2639acb19bc9e4162aaadbf
Peter Kastner <peter.kastner92@gmail.com>
2023-03-30 22:28:14 +0200

    Add PunchClockComposeApp composable main entry function

---
+++ app/src/main/java/com/pkapps/punchclock/PunchClockComposeApp.kt
@@ -0,0 +1,33 @@
+package com.pkapps.punchclock
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.pkapps.punchclock.navigation.PunchClockAppNavHost
+import com.pkapps.punchclock.ui.theme.PunchClockTheme
+
+@Composable
+fun PunchClockComposeApp(
+    modifier: Modifier = Modifier
+) {
+
+    PunchClockTheme {
+
+        val navController = rememberNavController()
+
+        Surface {
+
+            PunchClockAppNavHost(
+                navController = navController,
+                modifier = modifier.windowInsetsPadding(WindowInsets.displayCutout)
+            )
+
+        }
+
+    }
+
+}
