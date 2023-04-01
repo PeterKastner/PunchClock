e4e55dc15680fcfde71bb3fc2c7dd7e0003ece24
Peter Kastner <peter.kastner92@gmail.com>
2023-04-01 13:24:55 +0200

    Add initialization of timber logging to Application class

--- app/src/main/java/com/pkapps/punchclock/PunchClockApplication.kt
+++ app/src/main/java/com/pkapps/punchclock/PunchClockApplication.kt
@@ -2,8 +2,23 @@ package com.pkapps.punchclock
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class PunchClockApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize timber logging library when in debug mode
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                // log method name, line number and display it in logcat as a hyperlink
+                override fun createStackElementTag(element: StackTraceElement): String {
+                    return "(${element.fileName}:${element.lineNumber})#${element.methodName}"
+                }
+            })
+        }
+
+    }
 }
