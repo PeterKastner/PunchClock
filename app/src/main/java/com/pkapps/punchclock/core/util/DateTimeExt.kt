a105be684a922fd505b73a8d5cbd4a3281ef6762
Peter Kastner <peter.kastner92@gmail.com>
2023-04-02 21:19:11 +0200

    Add withLocale to DateTimeExt.kt

--- app/src/main/java/com/pkapps/punchclock/core/util/DateTimeExt.kt
+++ app/src/main/java/com/pkapps/punchclock/core/util/DateTimeExt.kt
@@ -3,10 +3,11 @@ package com.pkapps.punchclock.core.util
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import java.util.Locale
 
 fun LocalDateTime.toReadableTimeAsString(): String {
-    val pattern = "dd.MM.yyy HH:mm"
-    val formatter = DateTimeFormatter.ofPattern(pattern)
+    val pattern = "E dd.MM.yyy HH:mm"
+    val formatter = DateTimeFormatter.ofPattern(pattern).withLocale(Locale.getDefault())
 
     val truncated = this.truncatedTo(ChronoUnit.MINUTES)
 
