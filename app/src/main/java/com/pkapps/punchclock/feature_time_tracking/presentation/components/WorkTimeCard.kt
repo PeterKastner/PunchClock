5d8d9406ce3e5e0f2a6b6f4f5fdde8d6d8e2e9f6
Peter Kastner <peter.kastner92@gmail.com>
2023-04-02 20:47:39 +0200

    Add WorkTimeCard composable with preview and preview parameter provider class

---
+++ app/src/main/java/com/pkapps/punchclock/feature_time_tracking/presentation/components/WorkTimeCard.kt
@@ -0,0 +1,135 @@
+package com.pkapps.punchclock.feature_time_tracking.presentation.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import com.pkapps.punchclock.core.ui.theme.PunchClockTheme
+import com.pkapps.punchclock.core.util.toReadableTimeAsString
+import com.pkapps.punchclock.feature_time_tracking.data.local.WorkTime
+import java.time.Duration
+import java.time.LocalDateTime
+import java.util.*
+
+@Composable
+fun WorkTimeCard(
+    workTime: WorkTime,
+    modifier: Modifier = Modifier,
+    shape: Shape = shapes.medium,
+    elevation: CardElevation = CardDefaults.cardElevation(),
+    border: BorderStroke? = null,
+) {
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = shape,
+        elevation = elevation,
+        border = border
+    ) {
+
+        val style = typography.titleLarge.copy(
+            color = colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            textDecoration = TextDecoration.None
+        )
+
+        Column(
+            modifier = modifier
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth()
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth()
+            ) {
+
+                Text(
+                    text = "Start",
+                    style = style,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+                Text(
+                    text = workTime.start?.toReadableTimeAsString() ?: "",
+                    style = style,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = modifier
+                    .padding(vertical = 8.dp)
+                    .fillMaxWidth()
+            ) {
+
+                Text(
+                    text = "End",
+                    style = style
+                )
+                Text(
+                    text = workTime.end?.toReadableTimeAsString() ?: "",
+                    style = style
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "light preview", showBackground = true, showSystemUi = false)
+@Preview(name = "dark preview", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun WorkTimeCardPreview(
+    @PreviewParameter(WorkTimeProvider::class) workTime: WorkTime
+) {
+
+    PunchClockTheme {
+        Scaffold { innerPadding ->
+
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .padding(16.dp)
+            ) {
+
+                WorkTimeCard(
+                    workTime = workTime,
+                    modifier = Modifier.wrapContentSize(),
+                    shape = shapes.medium,
+                    elevation = CardDefaults.elevatedCardElevation(),
+                    border = BorderStroke(width = 2.dp, color = colorScheme.primary),
+                )
+            }
+        }
+    }
+
+}
+
+class WorkTimeProvider : PreviewParameterProvider<WorkTime> {
+    override val values = listOf(
+        WorkTime(
+            id = UUID.randomUUID(),
+            start = LocalDateTime.now(),
+            end = LocalDateTime.now() + Duration.ofHours(8),
+            pause = Duration.ZERO,
+            comment = ""
+        )
+    ).asSequence()
+}
