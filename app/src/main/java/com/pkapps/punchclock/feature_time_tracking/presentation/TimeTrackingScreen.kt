941699f976c3522adefe4d82d2b51bfac4947a3a
Peter Kastner <peter.kastner92@gmail.com>
2023-04-03 00:12:02 +0200

    Add csv file export via kotlin-csv, file provider and share intent

--- app/src/main/java/com/pkapps/punchclock/feature_time_tracking/presentation/TimeTrackingScreen.kt
+++ app/src/main/java/com/pkapps/punchclock/feature_time_tracking/presentation/TimeTrackingScreen.kt
@@ -1,5 +1,6 @@
 package com.pkapps.punchclock.feature_time_tracking.presentation
 
+import android.content.Intent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -13,12 +14,20 @@ import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import com.pkapps.punchclock.feature_time_tracking.presentation.components.DateHeader
 import com.pkapps.punchclock.feature_time_tracking.presentation.components.WorkTimeCard
+import kotlinx.coroutines.launch
+import java.io.File
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
+import java.time.temporal.ChronoUnit
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -39,6 +48,10 @@ fun TimeTrackingScreen(
         state.currentWorkTime.end,
         state.currentWorkTime.id
     ) { state.currentWorkTime.hasStart() && !state.currentWorkTime.hasEnd() }
+
+    val context = LocalContext.current
+
+    val scope = rememberCoroutineScope()
 
     Scaffold { innerPadding ->
 
@@ -74,6 +87,44 @@ fun TimeTrackingScreen(
                         enabled = state.workTimes.isNotEmpty(),
                         content = { Text(text = "Clear") }
                     )
+
+                    Button(
+                        onClick = {
+
+                            scope.launch {
+
+                                val csvFile = File(context.filesDir, "workTimes.csv")
+
+                                csvWriter().open(csvFile) {
+                                    writeRow(listOf("id", "start", "end", "pause", "comment"))
+                                    workTimesToDisplay.forEach {
+                                        writeRow(it.id, it.start!!.truncatedTo(ChronoUnit.MINUTES), it.end!!.truncatedTo(ChronoUnit.MINUTES), it.pause, it.comment)
+                                    }
+                                }
+
+                                val fileUri = FileProvider.getUriForFile(
+                                    context,
+                                    context.applicationContext.packageName + ".provider",
+                                    csvFile.absoluteFile
+                                )
+
+                                val intent = Intent().apply {
+                                    action = Intent.ACTION_SEND
+                                    type = "text/csv"
+                                    putExtra(Intent.EXTRA_SUBJECT, csvFile.name)
+                                    putExtra(Intent.EXTRA_STREAM, fileUri)
+                                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                                    addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+                                }
+
+                                val shareIntent =
+                                    Intent.createChooser(intent, "Share work times to")
+
+                                ContextCompat.startActivity(context, shareIntent, null)
+                            }
+                        },
+                        enabled = state.workTimes.isNotEmpty(),
+                        content = { Text("Share") })
                 }
             }
 
