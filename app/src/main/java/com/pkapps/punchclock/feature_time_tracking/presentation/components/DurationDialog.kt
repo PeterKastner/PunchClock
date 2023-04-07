8ca433aa2e4b3d599e3957f0b2c0a2e767d82c59
Peter Kastner <peter.kastner92@gmail.com>
2023-04-07 18:50:37 +0200

    Add DurationDialog for inputting break time

---
+++ app/src/main/java/com/pkapps/punchclock/feature_time_tracking/presentation/components/DurationDialog.kt
@@ -0,0 +1,44 @@
+package com.pkapps.punchclock.feature_time_tracking.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocalCafe
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import com.maxkeppeker.sheets.core.models.base.Header
+import com.maxkeppeker.sheets.core.models.base.IconSource
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
+import com.maxkeppeler.sheets.duration.models.DurationConfig
+import com.maxkeppeler.sheets.duration.models.DurationFormat
+import com.maxkeppeler.sheets.duration.models.DurationSelection
+import java.time.Duration
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DurationDialog(
+    duration: Duration,
+    closeSelection: () -> Unit,
+    onSubmit: (newDuration: Duration) -> Unit
+) {
+
+    val useCaseState = rememberUseCaseState(
+        visible = true,
+        onCloseRequest = { closeSelection() }
+    )
+
+    com.maxkeppeler.sheets.duration.DurationDialog(
+        state = useCaseState,
+        header = Header.Default(
+            title = "Input your break time",
+            icon = IconSource(Icons.Filled.LocalCafe)
+        ),
+        selection = DurationSelection { newTimeInSeconds ->
+            val durationToSubmit = Duration.ofSeconds(newTimeInSeconds)
+            onSubmit(durationToSubmit)
+        },
+        config = DurationConfig(
+            timeFormat = DurationFormat.HH_MM,
+            currentTime = duration.seconds
+        )
+    )
+
+}
